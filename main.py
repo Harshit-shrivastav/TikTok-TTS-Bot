6df99887fe756407949e6388dfc2d2a320551d47
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-05-08 19:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import io
 from telethon import TelegramClient, events, Button
 from functions import *
 
-API_ID = 4680197
-API_HASH = '495b0228624028d635bd748b22985f67'
-BOT_TOKEN = '5810975688:AAHc57W24SQu6_Nb9KnsW0eOxEgbsRmVImo'
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = str(os.environ.get("API_HASH"))
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 
 TEXT_BYTE_LIMIT = 300
 AUDIO_FORMAT = 'mp3'
@@ -26,21 +26,18 @@ async def text_input_handler(event):
         chunk = voices_list[i:i + 2]
         row = [Button.inline(voice_label, data=voices_map[str(index)].encode()) for index, voice_label in enumerate(chunk, start=i + 1)]
         voice_buttons.append(row)
-
-    await event.reply(f"You entered: {event.text}\nNow, select a voice:", buttons=voice_buttons)
+    await event.reply(f'Your Input: "{event.text}"\nNow, select a voice:', buttons=voice_buttons)
 
 @client.on(events.CallbackQuery())
 async def voice_selection_handler(event):
     user_id = event.sender_id
     selected_voice = event.data.decode()
     await event.answer(f"You selected: {selected_voice}")
-
     if user_id in user_text:
         text = user_text[user_id]
         if len(text.encode('utf-8')) > TEXT_BYTE_LIMIT:
             await event.reply(f"Text must not be over {TEXT_BYTE_LIMIT} UTF-8 characters.")
             return
-
         audio_data = generate_audio(text, selected_voice)
         if audio_data:
             audio_file = io.BytesIO(audio_data)
@@ -55,7 +52,7 @@ def main():
     if not check_service_availability():
         return
 
-    print("Telegram bot is running...")
+    print("Bot is running...\nHit ⭐ to github repo, if you liked my work.\nWritten by Harshit-shrivastav.")
     try:
         client.run_until_disconnected()
     except Exception as e:
